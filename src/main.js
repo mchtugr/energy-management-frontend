f10274b4fa31f0b1d030cc6766f0448bf7092d5c
murtazaaylak <murtazaaylak@gmail.com>
2021-12-12 13:45:23 +0300

    refactor: remove started files & add scss

--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import store from './store'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import './styles/main.scss'
+
 Vue.config.productionTip = false
+
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app')
