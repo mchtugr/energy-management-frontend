0c24694cb98b572bf52cbbe2c413faf65b773544
murtazaaylak <murtazaaylak@gmail.com>
2021-12-12 19:51:01 +0300

    feat: config vuex store for login

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,44 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: {
+      loading: false,
+      name: null,
+      role: null,
+      token: null,
+      error: null,
+    },
   },
   mutations: {
+    LOGIN_USER_LOADING(state) {
+      state.user.loading = true
+    },
+    LOGIN_USER_SUCCESS(state, payload) {
+      state.user = { ...payload, loading: false, error: null }
+    },
+    LOGIN_USER_ERROR(state, payload) {
+      state.user = { ...state.user, loading: false, error: payload }
+    },
   },
   actions: {
+    login(context, userObj) {
+      context.commit('LOGIN_USER_LOADING')
+      axios
+        .post('http://localhost:3000/api/users/login', userObj, {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+        .then((res) => {
+          context.commit('LOGIN_USER_SUCCESS', res.data)
+        })
+        .catch((err) => context.commit('LOGIN_USER_ERROR', err.response.data))
+    },
   },
-  modules: {
-  }
+  modules: {},
 })
