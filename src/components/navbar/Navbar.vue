fcf007dd463ffe827d714efafab10f9a0608e601
murtazaaylak <murtazaaylak@gmail.com>
2021-12-12 22:16:29 +0300

    feat: add navbar component

---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <b-navbar toggleable="lg" variant="light" fixed="top" class="px-3">
+    <b-navbar-brand class="navbar__logo p-0">
+      <router-link to="/">
+        <img src="@/assets/faradai-logo.svg" alt="logo" />
+      </router-link>
+    </b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav v-if="user.role === 'admin'">
+        <b-nav-item
+          ><router-link to="/dashboard">Dashboard</router-link></b-nav-item
+        >
+      </b-navbar-nav>
+      <b-navbar-nav class="navbar__right">
+        <b-nav-item-dropdown text="Lang" right class="dropdown-menu-end">
+          <b-dropdown-item>EN</b-dropdown-item>
+          <b-dropdown-item>TR</b-dropdown-item>
+        </b-nav-item-dropdown>
+        <b-nav-item-dropdown
+          :text="user.name | capitalize"
+          right
+          v-if="user.name"
+        >
+          <b-dropdown-item>Profile</b-dropdown-item>
+          <b-dropdown-item>Logout</b-dropdown-item>
+        </b-nav-item-dropdown>
+        <b-nav-item v-else>
+          <router-link to="/login">Login</router-link>
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import filters from '../../mixins/filters'
+export default {
+  name: 'Navbar',
+  computed: {
+    ...mapState(['user']),
+  },
+  mixins: [filters],
+}
+</script>
+
+<style lang="scss" src="./navbar.scss"></style>
