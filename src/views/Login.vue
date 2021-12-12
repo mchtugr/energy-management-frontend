ca3a3d71d0ef87490571363b94d13c88e757c4a2
murtazaaylak <murtazaaylak@gmail.com>
2021-12-12 17:56:09 +0300

    refactor: login page style

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,25 @@
 <template>
-  <div>LoginPage</div>
+  <div class="login-page">
+    <b-col lg="4" md="6" sm="8" cols="10" class="mx-auto">
+      <login-form />
+    </b-col>
+  </div>
 </template>
 
 <script>
+import LoginForm from '../components/login-form/LoginForm.vue'
 export default {
+  components: { LoginForm },
   name: 'Login',
 }
 </script>
+
+<style lang="scss">
+.login-page {
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  margin: auto;
+}
+</style>
