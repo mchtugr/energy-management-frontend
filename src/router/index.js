e670c55c9e9de02d0acc59895f18e53a05967eb2
murtazaaylak <murtazaaylak@gmail.com>
2021-12-12 22:32:40 +0300

    feat: add dashboard route

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,11 @@ const routes = [
     name: 'Login',
     component: () => import('../views/Login.vue'),
   },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: () => import('../views/Dashboard.vue'),
+  },
 ]
 
 const router = new VueRouter({
