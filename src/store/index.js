2669a8b713cdc60eb52c5bb1356e40ac511cd8b9
murtazaaylak <murtazaaylak@gmail.com>
2021-12-13 23:23:50 +0300

    feat: fetch factories vuex action & mutations

--- src/store/index.js
+++ src/store/index.js
@@ -13,6 +13,11 @@ export default new Vuex.Store({
       token: null,
       error: null,
     },
+    factories: {
+      loading: false,
+      error: null,
+      list: [],
+    },
   },
   mutations: {
     LOGIN_USER_LOADING(state) {
@@ -24,8 +29,18 @@ export default new Vuex.Store({
     LOGIN_USER_ERROR(state, payload) {
       state.user = { ...state.user, loading: false, error: payload }
     },
+    FETCH_FACTORIES_LOADING(state) {
+      state.factories.loading = true
+    },
+    FETCH_FACTORIES_SUCCESS(state, payload) {
+      state.factories = { list: payload, loading: false, error: null }
+    },
+    FETCH_FACTORIES_ERROR(state, payload) {
+      state.factories = { ...state.factories, loading: false, error: payload }
+    },
   },
   actions: {
+    // login user
     login(context, userObj) {
       context.commit('LOGIN_USER_LOADING')
       axios
@@ -38,6 +53,23 @@ export default new Vuex.Store({
           context.commit('LOGIN_USER_SUCCESS', res.data)
         })
         .catch((err) => context.commit('LOGIN_USER_ERROR', err.response.data))
+    },
+
+    // fetch all factories
+    fetchFactoryList(context) {
+      context.commit('FETCH_FACTORIES_LOADING')
+      axios
+        .get('http://localhost:3000/api/factories', {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+        .then((res) => {
+          context.commit('FETCH_FACTORIES_SUCCESS', res.data.data)
+        })
+        .catch((err) =>
+          context.commit('FETCH_FACTORIES_ERROR', err.response.data)
+        )
     },
   },
   modules: {},
