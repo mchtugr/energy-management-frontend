1c081c9a8f1e2e9da15a969df5c830cc5f37edf8
murtazaaylak <murtazaaylak@gmail.com>
2021-12-18 12:49:12 +0300

    feat: add verticalNav component for dashboard nav

---
+++ src/components/vertical-nav/VerticalNav.vue
@@ -0,0 +1,18 @@
+<template>
+  <b-nav vertical class="vertical-nav">
+    <b-nav-item to="/dashboard/factories" exact exact-active-class="active">
+      Factory List
+    </b-nav-item>
+    <b-nav-item to="/dashboard/units" exact exact-active-class="active">
+      Factory Units
+    </b-nav-item>
+  </b-nav>
+</template>
+
+<script>
+export default {
+  name: 'VerticalNav',
+}
+</script>
+
+<style lang="scss"></style>
