d323fc78e26006d4049a25477ca3e478b8e761a3
murtazaaylak <murtazaaylak@gmail.com>
2021-12-19 23:44:05 +0300

    feat: add signup route

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,11 @@ const routes = [
     name: 'Login',
     component: () => import('../views/Login.vue'),
   },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: () => import('../views/Signup.vue'),
+  },
 
   {
     path: '/dashboard/factories',
