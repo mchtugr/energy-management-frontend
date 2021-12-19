049e9371d955cd703546ad18be82338d9f321573
murtazaaylak <murtazaaylak@gmail.com>
2021-12-19 23:38:10 +0300

    feat: add signup page component

---
+++ src/views/Signup.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="signup-page">
+    <b-col lg="4" md="6" sm="8" cols="10" class="mx-auto">
+      <signup-form />
+    </b-col>
+  </div>
+</template>
+
+<script>
+import SignupForm from '../components/signup-form/SignupForm.vue'
+export default {
+  components: { SignupForm },
+  name: 'Login',
+}
+</script>
+
+<style lang="scss">
+.signup-page {
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 90vh;
+  margin: auto;
+}
+</style>
