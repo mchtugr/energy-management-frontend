afc089ab40f16d2a5386ecac58c0e8159ab20f6b
murtazaaylak <murtazaaylak@gmail.com>
2021-12-21 23:45:40 +0300

    feat: config i18next

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import store from './store'
+import i18n from './i18n'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -17,5 +18,6 @@ Vue.use(IconsPlugin)
 new Vue({
   router,
   store,
+  i18n,
   render: (h) => h(App),
 }).$mount('#app')
