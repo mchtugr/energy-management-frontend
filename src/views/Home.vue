bf31c0e7cc933a4be45f94e4acbd42f1029f8627
murtazaaylak <murtazaaylak@gmail.com>
2021-12-26 19:55:39 +0300

    feat: add homepage jumbotron and locale files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="home">HomePage</div>
+  <div class="home pt-5">
+    <b-jumbotron :header="$t('Welcome to Faradai Dashboard')" :lead="$t('You can select & see details of your factories and units')" class="mt-4">
+    <p>{{$t('For more information visit dashboard')}}</p>
+    <b-button variant="primary" @click="$router.push('/dashboard/factories')">{{$t('Dashboard')}}</b-button>
+  </b-jumbotron>
+  </div>
 </template>
 
 <script>
